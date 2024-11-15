f0773a6c5be1729e6d28a81c7f2e6bcc024c85ee
Ryan Knight <ryan.knight09@outlook.com>
2024-11-14 18:19:00 -0800

    moving back to npm

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,21 +2,18 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-11-14";
 
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET ||
-    process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET,
-  "Missing environment variable: SANITY_STUDIO_DATASET or NEXT_PUBLIC_SANITY_STUDIO_DATASET"
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID ||
-    process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: SANITY_STUDIO_PROJECT_ID or NEXT_PUBLIC_SANITY_PROJECT_ID"
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage);
   }
-
   return v;
 }
