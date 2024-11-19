ccf244a0e4a957251ecd1e3eff2d4bba08b34a5d
Ryan Knight <ryan.knight09@outlook.com>
2024-11-18 18:00:01 -0800

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,16 @@
 import { Title } from "@/components/Title";
-import { client } from "@/sanity/lib/client";
-import { EXPERIENCE_QUERY } from "@/sanity/lib/queries";
-import Image from "next/image";
 
 export default async function Page() {
-  const work = await client.fetch(EXPERIENCE_QUERY, {});
-
-  console.log(work[0].technologies[0].slug?.current);
-
   return (
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <Title>Layer Cake Home Page</Title>
-      <Image
+      {/* <Image
         className="dark:invert"
         src={`/svg/${work[0].technologies[0].slug?.current}.svg`}
         alt="Vercel logomark"
         width={100}
         height={100}
-      />
+      /> */}
     </section>
   );
 }
