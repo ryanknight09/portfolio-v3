f608f4ff87804e108ca1d2a79da2fb5c9a6837b1
Ryan Knight <ryan.knight09@outlook.com>
2024-12-12 13:14:22 -0800

    remove duplicate

--- components/about/Stack.tsx
+++ components/about/Stack.tsx
@@ -6,14 +6,7 @@ export const Stack = () => (
     <h1 className="text-xl md:text-2xl max-w-64 md:max-w-sm lg:max-w-xl break-words">
       Stack
     </h1>
-    <div className="text-muted-foreground text-sm overflow-auto">
-      <p className="break-word max-w-lg text-base">
-        Based in Spokane Washington, I&apos;m a front-end developer passionate
-        about building web apps that users love. When I&apos;m not coding, you
-        can usually find me making music. I love exploring new ways to combine
-        my technical skills with my creativity.
-      </p>
-    </div>
+    <div className="text-muted-foreground text-sm overflow-auto"></div>
     <DottedLineFlexContainer>
       <DottedLine className="max-w-3xl" />
       <AnimatedLink className="w-full md:max-w-max" href="stacks">
