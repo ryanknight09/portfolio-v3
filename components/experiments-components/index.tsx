550c9c0951126c7133a935f9739c363d27690ef2
Ryan Knight <ryan.knight09@outlook.com>
2024-12-18 20:53:38 -0800

    remove todo

--- components/experiments-components/index.tsx
+++ components/experiments-components/index.tsx
@@ -1,7 +1,3 @@
-import { DatePicker } from "./date-picker/DatePicker";
 import { TreeExample } from "./tree/TreeExample";
 
-export const experimentalComponents = [
-  <TreeExample key="tree" />,
-  <DatePicker key={"date-picker"} />,
-];
+export const experimentalComponents = [<TreeExample key="tree" />];
